0272084d13d60f02cc5a786a60b144940504a083
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2022-09-21 16:16:59 -0300

    Update LU factorization.cpp

--- LU factorization.cpp
+++ LU factorization.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-	cout<<fixed<<setprecision(5);
+	cout<<fixed<<setprecision(4);
     int n; cin>>n;
     queue<double>constantes;
     double A[n][n],B[n],L[n][n],U[n][n];
@@ -16,13 +16,15 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>B[i];
     }
+    double save;
     //Calculando a matriz U, e determinando as constantes para formar a matriz L
     for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
 			if(i>j){
+				save = (U[i][j]/U[j][j]);
 				constantes.push(U[i][j]/U[j][j]);
 				for(int k=j; k<n; k++){
-					U[i][k]=A[i][k]-A[j][k]*(A[i][j]/A[j][j]);
+					U[i][k]=U[i][k]-U[j][k]*save;
 				}
 			}
 		}
@@ -48,25 +50,29 @@ int main(){
         }
         Y.emplace_back(B[i]-soma);
     }
+    
+    //Calculando a matriz X, a partir de Ux=Y
     X.emplace_back(Y[n-1]/U[n-1][n-1]);
-    for(int i=n-2,k=0; i>=0; i--){
+    for(int i=n-2; i>=0; i--){
 		double soma=0;
+		int aux=0; 
 		for(int j=n-1; j>=0; j--){
 			if(i==j){
-				X.emplace_back(soma/U[i][j]*-1);//talvez de errado
-				k++;
+				X.emplace_back((Y[j]-soma)/U[i][j]);
 			}
 			else if(j>i){
-				soma+=(U[i][j]*X[k]-Y[k]);
+				soma+=U[i][j]*X[aux];
+				aux++;
 			}
 		}
 	}
+	
     cout<<"Valores de Y: "<<endl;
     for(int i=0; i<(int)Y.size(); i++){
         cout<<Y[i]<<endl;
     }
     cout<<"Valores de X: "<<endl;
-    for(int i=0; i<(int)X.size(); i++){
+    for(int i=(int)X.size()-1 ; i>=0; i--){
 		cout<<X[i]<<endl;
 	}   
     cout<<"Matriz L: "<<endl;
