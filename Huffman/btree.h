185f99fd46f06a3850793e05d996d0fc30ac6038
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-03-08 18:08:39 -0300

    Update btree.h

--- Huffman/btree.h
+++ Huffman/btree.h
@@ -6,6 +6,7 @@
 #include <utility>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 #define f first
 #define s second
@@ -17,11 +18,12 @@ class huffman{
 public:
 	node *root;
 	int n;
-	std::string OriText;               //texto original
-	std::string CodText;               //texto codificado
-	std::map<char, std::string>code;   //codigo de cara caracter
-	std::map<char, int>mp;
-	std::priority_queue<std::pair<int,node*>, std::vector<std::pair<int, node*>>, std::greater<std::pair<int,node*>> >pq;
+	string OriText;               //texto original
+	string CodText;               //texto codificado
+	map<char, std::string>code;   //codigo de cara caracter
+	map<char, int>mp;             //mapa das frequencias de cada caracter
+	vector<pair<int,char>>vec;    //frequencias dos caracteres de forma ordenada
+	priority_queue<std::pair<int,node*>, vector<std::pair<int, node*>>, greater<pair<int,node*>> >pq;
 	
 	void build(){
 		
@@ -29,6 +31,18 @@ public:
 		for(char k: OriText){
 			mp[k]++;
 		}
+		
+		//coloco os valores em uma priority para ordenar pela frequencia
+		for(auto [a, b]: mp){           
+			vec.push_back({b, a});
+		}
+		
+		//ordeno o vetor em ordem crescente pelo first e pelo second
+		sort(vec.begin(), vec.end(), [](pair<int,char>a, pair<int,char>b){
+			if(a.f == b.f)
+				return a.s < b.s;       
+			return a.f < b.f;
+		});
 		
 		//insiro os primeiros nodes na fila
 		for(auto [a, b]: mp){
@@ -74,9 +88,9 @@ public:
 				std::cout << "|\\n| " << b << "\n";
 				continue; 
 			} 
-			std::cout << "|" << a << "| " << b << "\n";
+			cout << "|" << a << "| " << b << "\n";
 		}
-		std::cout << "\n";
+		cout << "\n";
 	}
 	
 	void codifica(std::string s){
@@ -96,5 +110,18 @@ public:
 		}
 		dfs(&((*t)->l), path+"0");      //construo o caminho, se eu for para
 		dfs(&((*t)->r), path+"1");      //a esquerda adiciono 0 e para direita 1
+	}
+	
+	void frequencia(){
+		
+		cout << "Tabela de Frequencias:\n";
+		for(auto [a, b]: vec){
+			if(to_string(b) == "10"){//quebra de linha
+				std::cout << "|\\n| " << a << "\n";
+				continue; 
+			} 
+			cout << "|" << b << "| " << a << "\n";
+		}
+		cout << "\n";
 	}
 };
