2ef4caea474eca9bc7a4bbf4d616efa4fdb36415
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-03-08 08:24:57 -0300

    Update main.cpp

--- Huffman/main.cpp
+++ Huffman/main.cpp
@@ -10,50 +10,17 @@ using namespace std;
 
 int main(){
 	
-	tree huffman;
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	
 	char r;
-	map<char, int>mp;
-	priority_queue<pair<int,node*>, vector<pair<int, node*>>, greater<pair<int,node*>> >pq;
-	
-	//~ while(scanf("%c", &r)!=EOF){
-		//~ mp[r]++;
-		//~ huffman.OriText += r;
-	//~ }
-	
-	////////////////////////////
-	string str;
-	getline(cin,str);
-	for(char k: str){
-		mp[k]++;
-		str += k;
+	string s("");	
+	while(scanf("%c", &r)!=EOF){
+		s+=r;
 	}
+	huffman tree(s);
 	
-	//insiro os primeiros nodes na fila
-	for(auto [a, b]: mp){
-		node *c = new node;
-		c->leaf=1;
-		c->freq=b;
-		c->carac=a;
-		pq.push({b, c});
-	}
-	
-	while(pq.size()>1){
-		node *a = pq.top().s; pq.pop();
-		node *b = pq.top().s; pq.pop();
-		node *c = new node;
-		c->r = a;
-		c->l = b;
-		int k = a->freq + b->freq;
-		c->freq = k;  
-		pq.push({k, c});
-	}
-	
-	//insiro o node que constitui a arvore na raiz
-	huffman.root = pq.top().s;
-	pq.pop();
-	
-	
-	huffman.codifica(str);
-	huffman.dicionario();
-	cout << huffman.CodText << endl;
+	tree.dicionario();
+	cout << "Arquivo compactado:\n";
+	cout << tree.CodText << endl;
 }
