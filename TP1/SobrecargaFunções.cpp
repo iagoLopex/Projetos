a43f6da0c29de7ebfa47d517a0139697b3a034d3
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-03-27 21:32:30 -0300

    Create SobrecargaFunções.cpp

---
+++ TP1/SobrecargaFunções.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(double c){
+	cout << "double " << c << endl;
+}
+
+void print(int c){
+	cout << "int " << c << endl;
+}
+
+void print(char const *c){
+	cout << "char* " << c << endl;
+}
+
+void print(string c){
+	cout << "string " << c << endl;
+}
+
+int main(){
+	
+	//print(5);
+	//print(5.5);
+	//print("teste");
+	//string s = "asd";
+	//print(s);
+	
+}
