1c770e00ab18f86a9f5cb0e911a4d8ace8c1d6e0
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-04-05 10:29:01 -0300

    Create main.cpp

---
+++ ED2/Huffman/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#include "btree.h"
+#include "node.h"
+
+#define f first
+#define s second
+
+using namespace std;
+
+int main(){
+	
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	
+	char r;
+	string s("");	
+	while(scanf("%c", &r)!=EOF){
+		s+=r;
+	}
+	huffman tree(s);
+	
+	tree.frequencia();
+	tree.dicionario();
+	cout << "Arquivo compactado:\n";
+	cout << tree.CodText << endl;
+}
