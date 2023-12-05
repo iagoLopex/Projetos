8ff500de129d5e70eb64b0f1380e70b2f608fe96
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-12-05 17:39:18 -0300

    Update simulador_ethernet.c

--- SO1/simulador_ethernet.c
+++ SO1/simulador_ethernet.c
@@ -1,1 +1,74 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h> 
 
+#define ANSI_COLOR_RED "\x1b[31m" 
+#define ANSI_COLOR_GREEN "\x1b[32m" 
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+int backofftime=10,waittime=10;
+struct timeval lasttime;
+struct timeval currenttime;
+int status(3];
+
+init()
+{ 
+	int i; 
+	for(i=0;i<3;i++) 
+		status[i]=0; 
+}
+
+void csma(void *station)
+{ 
+	int name,l=3,t=0,i; 
+	name=atoi(station); 
+	while(1){ 
+		if(status[0]+status[1]+status[2]>=3) 
+			exit(0);
+
+		if (status [name]==1){
+			gettimeofday(&currenttime,0);
+			sleep(waittime);
+		}
+		else{
+			
+			printf("Station %d transmitting a frame\n",name);
+			gettimeofday(&currenttime,0);
+			printf("Station %d: last time: %d , current time:%d\n",name,lasttime.tv_sec,currenttime.tv_sec);
+			usleep(waittime);
+			
+			if(lasttime.tv_sec+backofftime >= currenttime.tv_sec && status[name]==0){
+				
+				printf(ANSI_COLOR_RED "Station %d : Collision Detected :("ANSI_COLOR_RESET "\n",name);
+				l--;
+				
+				if(l>0){
+					printf("Station %d: data resending, attemptsremaining=%d\n",name,l);
+					lasttime.tv_sec=currenttime.tv_sec;
+					sleep(waittime);
+				}
+				else{
+					printf("Station %d: limit expired\n",name);
+				}
+			}
+			else{
+				
+				if(status[name]==0){
+					lasttime.tv_sec=currenttime.tv_sec;
+					printf(ANSI_COLOR_GREEN "Station %d : Data SendSuccess!!!" ANSI_COLOR_RESET "\n",name);
+					status[name]=1;
+				}
+				lasttime.tv_sec=currenttime.tv_sec;
+			}
+			
+			sleep(waittime);
+		}
+	}
+	//...
+	
+}
+	
+	
+	
+	
+	
